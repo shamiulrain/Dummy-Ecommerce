29afbe01cdb1da0dee49171ee364bb292690d852
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2018-06-30 12:30:55 +0600

    Revert "AFNetworking framework added"
    This reverts commit a8b5dbb7112626c10171d60878bc7dd61d4fd5b8.

--- Dummy-Ecommerce/Pods/Target Support Files/Pods-Dummy-Ecommerce/Pods-Dummy-Ecommerce-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dummy_Ecommerce : NSObject
-@end
-@implementation PodsDummy_Pods_Dummy_Ecommerce
-@end
--- Dummy-Ecommerce/Pods/Target Support Files/Pods-Dummy-EcommerceTests/Pods-Dummy-EcommerceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dummy_EcommerceTests : NSObject
-@end
-@implementation PodsDummy_Pods_Dummy_EcommerceTests
-@end
