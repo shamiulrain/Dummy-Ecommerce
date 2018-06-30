715b180e1c2f45c415050aef1869ee4a38574b2e
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2018-06-30 11:34:18 +0600

    New Xcode project created

---
+++ Dummy-Ecommerce/Dummy-Ecommerce/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dummy-Ecommerce
+//
+//  Created by Tanjim on 30/6/18.
+//  Copyright © 2018 Tanjim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dummy-Ecommerce/Dummy-EcommerceTests/Dummy_EcommerceTests.m
@@ -0,0 +1,39 @@
+//
+//  Dummy_EcommerceTests.m
+//  Dummy-EcommerceTests
+//
+//  Created by Tanjim on 30/6/18.
+//  Copyright © 2018 Tanjim. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dummy_EcommerceTests : XCTestCase
+
+@end
+
+@implementation Dummy_EcommerceTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
